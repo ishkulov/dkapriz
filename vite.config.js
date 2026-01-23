9e1d1c0907339f175c1b780862b00db51ac029b4
Иларион Ишкулов <ishkulov@gmail.com>
2026-01-23 16:12:01 +0300

    Fix hash inconsistency with vite-imagetools

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { imagetools } from 'vite-imagetools'
-import viteImagemin from 'vite-plugin-imagemin'
-
-// Если viteImagemin undefined (для ESM)
-const ViteImagemin = viteImagemin.default || viteImagemin
 
 export default defineConfig({
   plugins: [
-    vue(),
     imagetools(), // поддержка ?meta, ?webp, ?avif и др.
-    ViteImagemin({
-      mozjpeg: { quality: 75 },
-      optipng: { optimizationLevel: 5 },
-      pngquant: { quality: [0.7, 0.9] },
-      webp: { quality: 75 },
-      svgo: {},
-    }),
+    vue(),
   ],
   base: '/',
   resolve: {
@@ -33,6 +22,9 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    commonjsOptions: {
+      strictRequires: true
+    }
   },
   ssgOptions: {
     script: 'async',
